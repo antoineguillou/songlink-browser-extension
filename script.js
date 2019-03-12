3444af30b574f9c0f2baa1722c2abe08c1256b19
Antoine Guillou <contact@antoineguillou.fr>
2019-03-12 18:02:55 +0100

    update icon opacity on tab change + bugfix

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-var options = {}
+var validUrls = [
+  'play.google.com/music',
+  'open.spotify.com',
+  'itunes.apple.com',
+  'deezer.com',
+  'listen.tidal.com',
+  'soundcloud.com',
+  'pandora.com/artist/',
+  'music.yandex.com',
+  'youtube.com/watch'
+];
+var prependUrl = 'https://song.link/';
+
+var updateTabIcon= function(){
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    chrome.pageAction.hide(tabs[0].id);
+    validUrls.forEach(function(e){
+      if(url.indexOf(e) > -1){
+        chrome.pageAction.show(tabs[0].id);
+      }
+    });
+  });
+}
 
 var copyToClipboard = function(text) {
   var selected = false;
@@ -37,7 +60,7 @@ var getGPMURL = function(tabs){
     var id = response.id;
     if(id !== null){
       var shareurl = "https://play.google.com/music/m/"+id;
-      buttonActions('https://song.link/'+encodeURI(shareurl));
+      buttonActions(prependUrl+encodeURI(shareurl));
     }
   });
 }
@@ -47,9 +70,11 @@ var getSpotifyURL = function(tabs){
   var url = tabs[0].url;
   var re = /\/album\/([a-zA-Z0-9]+)/g;
   var match = re.exec(url);
-  var id = match[1];
-  var shareurl = "https://open.spotify.com/album/"+id;
-  buttonActions('https://song.link/'+encodeURI(shareurl));
+  if(match){
+    var id = match[1];
+    var shareurl = "https://open.spotify.com/album/"+id;
+    buttonActions(prependUrl+encodeURI(shareurl));
+  }
 }
 
 // Apple Music
@@ -57,9 +82,11 @@ var getAppleMusicURL = function(tabs){
   var url = tabs[0].url;
   var re = /\/album\/\S+\/([a-zA-Z0-9]+)/g;
   var match = re.exec(url);
-  var id = match[1];
-  var shareurl = "https://itunes.apple.com/album/"+id;
-  buttonActions('https://song.link/'+encodeURI(shareurl));
+  if(match){
+    var id = match[1];
+    var shareurl = "https://itunes.apple.com/album/"+id;
+    buttonActions(prependUrl+encodeURI(shareurl));
+  }
 }
 
 // Deezer
@@ -67,18 +94,30 @@ var getDeezerURL = function(tabs){
   var url = tabs[0].url;
   var re = /\/album\/([a-zA-Z0-9]+)/g;
   var match = re.exec(url);
-  var id = match[1];
-  var shareurl = "https://www.deezer.com/en/album/"+id;
-  buttonActions('https://song.link/'+encodeURI(shareurl));
+  if(match){
+    var id = match[1];
+    var shareurl = "https://www.deezer.com/en/album/"+id;
+    buttonActions(prependUrl+encodeURI(shareurl));
+  }
 }
 
 // Use full URL
 var getCurrentUrl = function(tabs){
   var url = tabs[0].url;
-  buttonActions('https://song.link/'+encodeURI(url));
+  buttonActions(prependUrl+encodeURI(url));
 }
 
-chrome.browserAction.onClicked.addListener(function(tab){
+// Update Icon if URL is valid
+chrome.tabs.onActivated.addListener(function(tab){
+  updateTabIcon();
+});
+
+chrome.tabs.onUpdated.addListener(function(tab){
+  updateTabIcon();
+});
+
+// Click action
+chrome.pageAction.onClicked.addListener(function(tab){
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
 
